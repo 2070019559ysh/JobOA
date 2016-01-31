b385050988828f5523ceaba57c77ecdeb8440d0b
YSH <tow070019559@163.com>
2016-01-31 23:28:03 +0800

    feat:添加项目管理主页面
    添加页面和角色权限路径

--- data/AccessPath.sql
+++ data/AccessPath.sql
@@ -4,10 +4,12 @@ INSERT INTO AccessPath(HttpMethod,[Path])
 SELECT 'GET','/Home/Index' UNION
 SELECT 'GET','/Account/Index' UNION
 SELECT 'GET','/AdminTask/Index' UNION
-SELECT 'GET','/AdminHome/Index'
+SELECT 'GET','/AdminHome/Index' UNION
+SELECT 'GET','/AdminProject/Index'
 
 INSERT INTO Permission([Description],AccessPathId)
 SELECT '访问首页',1 UNION
 SELECT '登录',2 UNION
 SELECT '任务管理主页',3 UNION
-SELECT '管理页面主页',4
+SELECT '管理页面主页',4 UNION
+SELECT '公司项目管理主页',5
