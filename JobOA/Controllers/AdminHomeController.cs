56ad96b155e128fed1132a6e9f8cf12015990d67
YSH <tow070019559@163.com>
2016-04-23 00:05:53 +0800

    feat:实现动态显示公告信息
    选择菜单记录用户的选项基本保持原选择状态

--- JobOA/Controllers/AdminHomeController.cs
+++ JobOA/Controllers/AdminHomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using JobOA.BLL;
 using Ninject;
 using JobOA.Model;
+using System.Text;
 
 namespace JobOA.Controllers
 {
@@ -14,11 +15,23 @@ namespace JobOA.Controllers
     /// </summary>
     public class AdminHomeController : Controller
     {
+        /// <summary>
+        /// 依赖注入员工信息管理对象
+        /// </summary>
         [Inject]
         public IEmployeeManager EmployeeManager { get; set; }
+
+        /// <summary>
+        /// 依赖注入系统界面信息管理对象
+        /// </summary>
+        [Inject]
+        public IOAUiManager OAUiManager { get; set; }
         //
         // GET: /AdminHome/
-
+        /// <summary>
+        /// GET请求进入后台开始工作首页
+        /// </summary>
+        /// <returns>开始工作首页视图</returns>
         public ActionResult Index()
         {
             return View();
@@ -46,6 +59,28 @@ namespace JobOA.Controllers
                 }
             });
             return Json(new { online = onLineEmp, offline = offLineEmp });
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public MvcHtmlString LoadRemark()
+        {
+            //获取系统界面信息
+            List<OAUi> noticeOauiList = OAUiManager.SearchOauiByType("joboa_System_Notice",6);
+            StringBuilder htmlContent =new StringBuilder();
+            //传递给界面显示
+            if (noticeOauiList != null)
+            {
+                foreach (var oaui in noticeOauiList)
+                {
+                    htmlContent.AppendFormat("<div class=\"am-panel am-panel-default admin-sidebar-panel\"><div class=\"am-panel-bd\"><p><span class=\"am-icon-bookmark\"></span> &nbsp;{0}</p><p>{1}</p></div></div>",
+                        oaui.UiTitle,oaui.UiMess);
+                }
+            }else{
+                htmlContent.Append("<div class=\"am-panel am-panel-default admin-sidebar-panel\"><div class=\"am-panel-bd\"><p><span class=\"am-icon-bookmark\"></span> &nbsp;公告</p><p>时光静好，与君语；细水流年，与君同。—— Amaze UI</p></div></div>");
+            }
+            MvcHtmlString htmlString = new MvcHtmlString(htmlContent.ToString());
+            return htmlString;
         }
     }
 }
---
+++ JobOA/Scripts/js/cookie.js
@@ -0,0 +1,48 @@
+﻿var Cookie = (function () {
+    /* 设置cookie，time以毫秒为单位
+     * @param name cookie的名字
+     * @param value cookie的值
+     * @param time cookie距离现在的有效时间（毫秒）
+     * @return undefined
+     */
+    function setCookie(name, value, time) {
+        var exdate = new Date();
+        exdate.setTime(exdate.getTime() + time);//time以毫秒为单位
+        //如果时间不为空则设置过期时间
+        document.cookie = name + "=" + encodeURIComponent(value) + (time != null ? ";expires=" + exdate.toUTCString() : "");
+    }
+    /* 获取cookie
+     * @param name cookie的名字
+     * @return String cookie的值
+     */
+    function getCookie(name) {
+        if (document.cookie.length > 0) {
+            var c_start = document.cookie.indexOf(name + "=");
+            if (c_start !== -1) {
+                c_start = c_start + name.length + 1;
+                var c_end = document.cookie.indexOf(";", c_start);
+                if (c_end === -1) {
+                    c_end = document.cookie.length;
+                }
+                return decodeURIComponent(document.cookie.substring(c_start, c_end));
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+    /* 删除cookie
+     * @return undefined
+     */
+    function delCookie(name) {
+        if (getCookie(name) != null) {
+            setCookie(name, "", -6000);//设置一个过期的cookie值
+        }
+    }
+    return {
+        setCookie: setCookie,
+        getCookie: getCookie,
+        delCookie: delCookie
+    };
+})();
